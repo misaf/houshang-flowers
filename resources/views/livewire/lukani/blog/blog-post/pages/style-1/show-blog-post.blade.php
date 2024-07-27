7e5edc1102ea492637624378880a83597e8068c3
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-07-27 15:09:38 +0330

    update

--- resources/views/livewire/lukani/blog/blog-post/pages/style-1/show-blog-post.blade.php
+++ resources/views/livewire/lukani/blog/blog-post/pages/style-1/show-blog-post.blade.php
@@ -32,46 +32,47 @@
         <div class="absolute bottom-0 left-0 z-50 w-full h-16 bg-green-900">
             <x-lukani.sub-menu />
         </div>
+    </div>
 
-        <!-- Left div -->
-        <div class="w-full lg:w-2/3 bg-white p-4 overflow-y-auto lg:ms-auto lg:relative lg:z-0">
-            <div class="grid grid-cols-1 gap-4">
-                @if (!empty($blogPost['data'][0]) && !empty($blogPost['included']))
-                    <div id="controls-carousel" class="relative w-full" data-carousel="static">
-                        <!-- Carousel wrapper -->
-                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                            <!-- Item 2 -->
-                            <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                                <x-responsive-image :results="$blogPost" :item="$blogPost['data'][0]" :alt="$blogPost['data'][0]['attributes']['name']" :title="$blogPost['data'][0]['attributes']['name']" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-                            </div>
-                            <!-- Item 3 -->
-                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                <x-responsive-image :results="$blogPost" :item="$blogPost['data'][0]" :alt="$blogPost['data'][0]['attributes']['name']" :title="$blogPost['data'][0]['attributes']['name']" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-                            </div>
+    <!-- Left div -->
+    <div class="w-full lg:w-2/3 bg-white p-4 overflow-y-auto lg:ms-auto lg:relative lg:z-0">
+        <div class="grid grid-cols-1 gap-4">
+            @if (!empty($blogPost['data'][0]) && !empty($blogPost['included']))
+                <div id="controls-carousel" class="relative w-full" data-carousel="static">
+                    <!-- Carousel wrapper -->
+                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                        <!-- Item 2 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                            <x-responsive-image :results="$blogPost" :item="$blogPost['data'][0]" :alt="$blogPost['data'][0]['attributes']['name']" :title="$blogPost['data'][0]['attributes']['name']" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
                         </div>
-                        <!-- Slider controls -->
-                        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                                </svg>
-                                <span class="sr-only">Previous</span>
-                            </span>
-                        </button>
-                        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                                </svg>
-                                <span class="sr-only">Next</span>
-                            </span>
-                        </button>
+                        <!-- Item 3 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <x-responsive-image :results="$blogPost" :item="$blogPost['data'][0]" :alt="$blogPost['data'][0]['attributes']['name']" :title="$blogPost['data'][0]['attributes']['name']" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+                        </div>
                     </div>
+                    <!-- Slider controls -->
+                    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                    </button>
+                    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+                    </button>
+                </div>
 
-                    <div>{!! $blogPost['data'][0]['attributes']['description'] !!}</div>
-                @else
-                    <p class="text-center text-gray-500">No blog posts available.</p>
-                @endif
-            </div>
+                <div>{!! $blogPost['data'][0]['attributes']['description'] !!}</div>
+            @else
+                <p class="text-center text-gray-500">No blog posts available.</p>
+            @endif
         </div>
     </div>
+</div>
