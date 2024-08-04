55f1c20ee50550e8c2d4f1e6ec4c3939cbd5a2d9
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-08-04 10:03:37 +0330

    update

--- resources/views/components/responsive-image.blade.php
+++ resources/views/components/responsive-image.blade.php
@@ -2,8 +2,8 @@
 
 <div>
     <picture>
-        <source srcset="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-small.webp') }}" media="(max-width: 640px)">
-        <source srcset="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-medium.webp') }}" media="(max-width: 1024px)">
+        <source srcset="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-medium.webp') }}" media="(max-width: 640px)">
+        <source srcset="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-large.webp') }}" media="(max-width: 1024px)">
         <source srcset="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-large.webp') }}" media="(min-width: 1025px)">
         <img src="{{ \Storage::disk('panel')->url($image['attributes']['uuid'] . '/conversions/' . $fileName . '-extra-large.webp') }}" {{ $attributes }}>
     </picture>
