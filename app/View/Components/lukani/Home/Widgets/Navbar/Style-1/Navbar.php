9ffd8e78353becfe4d063e5b090467a8e294d95f
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-08-04 11:09:27 +0330

    update

---
+++ app/View/Components/lukani/Home/Widgets/Navbar/Style-1/Navbar.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\View\Components\lukani\Home\Widgets\Navbar\Style1;
+
+use App\Services\Api\Product\ProductCategoryService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\View\Component;
+
+final class Navbar extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct() {}
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View
+     */
+    public function render(): View
+    {
+        $productCategories = (new ProductCategoryService())->listProductCategories(
+            queryParams: [
+                'fields[product-categories]' => 'name,description',
+                'filter[status]'            => true,
+                'sort'                      => '-position',
+                'page[size]'                => 12,
+            ],
+        );
+
+        return view('components.lukani.home.widgets.navbar.style-1.navbar', compact('productCategories'));
+    }
+}
