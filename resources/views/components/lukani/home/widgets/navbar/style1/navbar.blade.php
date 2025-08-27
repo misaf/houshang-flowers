73c1b51acec9db76a7ce2d6ac12ab6e236e0988b
Ehsan Mahmoodi <misaf.1990@gmail.com>
2025-08-27 13:11:16 +0330

    update

--- resources/views/components/lukani/home/widgets/navbar/style1/navbar.blade.php
+++ resources/views/components/lukani/home/widgets/navbar/style1/navbar.blade.php
@@ -13,17 +13,16 @@
         </a>
         <div class="hidden md:flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <div class="gap-x-2 flex">
-                <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="tracking-wide p-2 px-4 items-center justify-between flex flex-wrap text-sm text-white bg-green-900 md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" type="button">
+                {{-- <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="tracking-wide p-2 px-4 items-center justify-between flex flex-wrap text-sm text-white bg-green-900 md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" type="button">
                     <x-heroicon-s-user class="w-4 h-4 me-2 text-white" />
                     {{ __('ورود به حساب کاربری') }}
-                </button>
+                </button> --}}
 
                 <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                     <div class="relative p-4 w-full max-w-md max-h-full">
                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 class="text-xl font-semibold text-green-900 dark:text-white">
-                                    {{ __('ورود به حساب کاربری') }}
                                 </h3>
                                 <button type="button" class="end-2.5 text-green-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
                                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
--- resources/views/livewire/lukani/product/product-category-filter.blade.php
+++ resources/views/livewire/lukani/product/product-category-filter.blade.php
@@ -26,7 +26,7 @@
                 @endphp --}}
                     <li>
                         <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                            <input class='w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' id="{{ $productCategory['attributes']['slug'] }}" type="checkbox" value="{{ $productCategory['attributes']['slug'] }}" wire:model.change="selected">
+                            <input class='w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500' id="{{ $productCategory['attributes']['slug'] }}" type="radio" value="{{ $productCategory['attributes']['slug'] }}" wire:model.change="selected">
                             <label for="{{ $productCategory['attributes']['slug'] }}" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">
                                 {{ $productCategory['attributes']['name'] }}
                             </label>
