0b9da3fc087cc4c7481240f3c189025045f31fd5
Misaf <misaf.1990@gmail.com>
2026-05-10 14:29:47 +0330

    removed

--- resources/views/livewire/lukani/pages/style-1/home.blade.php
+++ resources/views/livewire/lukani/pages/style-1/home.blade.php
@@ -7,7 +7,5 @@
 
     <x-lukani.home.widgets.intro.style1.intro />
 
-    <livewire:lukani.blog.blogPost.widgets.style-1.latest-blog-posts :lazy="false" />
-
     <livewire:lukani.newsletter.widgets.style-1.newsletter :lazy="false" />
 </div>
